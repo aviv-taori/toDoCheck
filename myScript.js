c94ed619e649d00220c1249e2c56bae8625ba809
aviv-taori <134504658+aviv-taori@users.noreply.github.com>
2023-09-12 11:17:29 +0300

    end no css

--- myScript.js
+++ myScript.js
@@ -1,10 +1,25 @@
-let text =""
+let text ="";
+let mystyley ;
 
-function clickCheck(){
+function clickNewCheck(){
    let value1 = document.getElementById("addTask").value;
    console.log(value1)
    text += 
-           ` <input type="checkbox" id="${value1}" name="${value1}" value="${value1}">
-            <label for="${value1}" class="checks> ${value1}</label><br>`
+           ` <input type="checkbox" id="${value1}" name="${value1}" value="${value1}" onclick="clickOnCheck('${value1}')">
+            <label id="${value1}lab"  for="${value1}" class="checks" style="text-decoration: none;"> ${value1}</label><br>`
    document.getElementById("change").innerHTML = text;
+}
+
+function clickOnCheck(value2){
+    
+
+    let checkBox = document.getElementById(value2);
+    
+    let deltext = document.getElementById(`${value2}lab`);
+
+    if (checkBox.checked == true){
+        deltext.style.textDecoration = "line-through" 
+      } else if (checkBox.checked != true){
+        deltext.style.textDecoration = "none"  
+      }
 }
